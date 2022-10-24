9236fd63527fd2d22d6b6138e36cfd1cd5065ba5
joe_bennett <joe.d.bennett01@gmail.com>
2022-10-24 14:19:05 -0500

    update file

---
+++ env.py
@@ -0,0 +1,10 @@
+host = "data.codeup.com"
+user = "leavitt_1875"
+password = "Pc4PrcXnnOhMCP8HmtEMaGgKD79V8xJ6"
+
+
+def get_db_url(db_name):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
+
+github_token='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'
+github_username='joe-bennett'
